70d2c15b4c6541abc3c01bbf1ba58dc010c6b569
pitkajuh <github_pj@protonmail.com>
2024-06-09 23:12:10 +0300

    Edit

--- src/decay.rs
+++ src/decay.rs
@@ -2,22 +2,36 @@ use crate::nuclide::Nuclide;
 use crate::nuclide::RadioNuclide;
 use crate::nuclide::get_activity;
 
+// pub trait Particle
+// {
+
+// }
+
 pub struct Electron
 {
     pub energy: f64,
 }
 
-// pub struct Decay<'a>
-// {
-//     pub parent_nuclide: RadioNuclide,
-//     pub daughter_nuclide: &'a dyn Nuclide, // Depends on the reaction, whether the produced is stable or unstable.
-//     pub product_particle: Vec<Particle>,
-// }
+pub struct Gamma
+{
+    pub energy: f64,
+}
+
+pub trait Decay
+{
+
+
+}
 
 pub struct BetaDecay<'a>
 {
     pub parent_nuclide: RadioNuclide,
-    pub daughter_nuclide: &'a dyn Nuclide, // Depends on the reaction, whether the produced is stable or unstable.
-    pub electron: Electron,
-    // Antineutrino is omitted.
+    pub daughter_nuclide: Vec<&'a dyn Nuclide>,
+    pub electron: Vec<Electron>
+}
+
+pub struct DecayScheme
+{
+
+
 }
