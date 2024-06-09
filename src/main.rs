3f26f1e49f36386442d63141da84d60000e47e09
pitkajuh <github_pj@protonmail.com>
2024-06-09 15:57:26 +0300

    Edit

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,117 @@
 use core::f64;
 
-pub struct Particle
+trait Nuclide
 {
-    energy: f64,
+    fn exp_law(&self, _time: f64)->f64
+    {
+	0.0.to_owned()
+    }
+
+    fn print_activity(&self, time: f64);
+
+    // fn new()->Self;
 }
 
-pub struct Decay
-{
-    product_nuclide: RadioNuclide,
-    product_particle: Particle,
-}
-
-pub struct RadioNuclide
+struct StableNuclide
 {
     name: String,
     half_life: f64,
     activity: f64,
 }
 
-impl RadioNuclide
+struct RadioNuclide
 {
-    pub fn exp_law(&self, time: f64)->f64
+    name: String,
+    half_life: f64,
+    activity: f64,
+}
+
+impl Nuclide for StableNuclide
+{
+    // fn new(name: String, half_life: f64, activity: f64) ->StableNuclide
+    // {
+    // 	StableNuclide{name, half_life, activity}
+    // }
+
+    fn exp_law(&self, _time: f64)->f64
+    {
+	self.activity
+    }
+
+    fn print_activity(&self, time: f64)
+    {
+	println!("Activity of {} is {}", self.name, self.exp_law(time));
+    }
+
+    // fn get_activity(nuclide: &dyn Nuclide, time: f64)
+    // {
+    // 	nuclide.print_activity(time);
+    // }
+}
+
+impl Nuclide for RadioNuclide
+{
+    fn exp_law(&self, time: f64)->f64
     {
 	let a: f64=-f64::consts::LN_2*time/self.half_life;
-	let b: f64=self.activity*a.exp();
-	return b;
+	self.activity*a.exp()
+    }
+
+    fn print_activity(&self, time: f64)
+    {
+	println!("Activity of {} is {}", self.name, self.exp_law(time));
     }
 }
+
+    fn get_activity(nuclide: &dyn Nuclide, time: f64)
+    {
+	nuclide.print_activity(time);
+    }
+
+
+// pub struct Particle
+// {
+//     energy: f64,
+// }
+
+// pub struct Reaction
+// {
+//     product_nuclide: Nuclide,
+//     // product_particle: Particle,
+// }
+
+// fn get_activity(radionuclide: &dyn Nuclide, time: f64)->f64
+// {
+//     return radionuclide.exp_law(time);
+// }
+
+
+
+// fn print_activity(nuclide: &dyn Nuclide, time: f64)
+// {
+//     println!("Activity of {} is {} ", nuclide.name, nuclide. get_activity(time));
+// }
+
+// fn print_activity(nuclide: RadioNuclide, time: f64)
+// {
+//     println!("Activity of {} is {} ", nuclide.name, nuclide. get_activity(time));
+// }
+
+
 
 fn main() {
     println!("Hello, world!");
 
-    let cs_137 = new1(String::from("Cs-137"), 30.0, 50.0);
-    let ba_137m=RadioNuclide {name: String::from("Ba-137m"), half_life: 2.55/(60.0*365.0), activity: 100.0,};
-    let ba_137=RadioNuclide {name: String::from("Ba-137"), half_life: 0.0, activity: 100.0,};
+    let cs_137=new1(String::from("Cs-137"), 30.0, 50.0);
+    let ba_137m=RadioNuclide {name: String::from("Ba-137m"), half_life: 2.55, activity: 100.0,};
+    let ba_137=StableNuclide {name: String::from("Ba-137"), half_life: 0.0, activity: 100.0,};
 
-    println!("Activity is {}", cs_137.exp_law(10.0));
-    println!("Activity is {}", ba_137m.exp_law(10.0));
-
+    // cs_137.print_activity(10.0);
+    // ba_137m.print_activity(1.0);
+    // ba_137.print_activity(10.0);
+    get_activity(&cs_137, 10.0);
+    get_activity(&ba_137m, 100.0);
+    get_activity(&ba_137, 10.0);
 }
 
 fn new1(name: String, half_life: f64, activity: f64)->RadioNuclide
