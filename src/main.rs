cb5b1cd18cef973879e215652efd95b1c957ec34
pitkajuh <github_pj@protonmail.com>
2024-06-09 16:46:22 +0300

    Add StableNuclide

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,19 @@ use core::f64;
 
 trait Nuclide
 {
-    fn exp_law(&self, _time: f64)->f64
-    {
-	0.0.to_owned()
-    }
+    // fn exp_law(&self, _time: f64)->f64
+    // {
+    // 	0.0.to_owned()
+    // }
 
-    fn print_activity(&self, time: f64);
+    // fn print_activity(&self, time: f64);
 
-    fn new(&self);//->Self;
+    // fn new(&self);
+    // fn new(&self, name: String, half_life: f64, activity: f64)->&dyn Nuclide;
+    // fn new(&self);//->&dyn Nuclide;//->Self;
+
+    // fn new()->dyn Nuclide;
+    fn new(name: String, half_life: f64, activity: f64)->Self;
 }
 
 struct StableNuclide
@@ -19,33 +24,44 @@ struct StableNuclide
     activity: f64,
 }
 
-struct RadioNuclide
-{
-    name: String,
-    half_life: f64,
-    activity: f64,
-}
+// struct RadioNuclide
+// {
+//     name: String,
+//     half_life: f64,
+//     activity: f64,
+// }
 
 impl Nuclide for StableNuclide
 {
+    fn new(name: String, half_life: f64, activity: f64)->StableNuclide
+    {
+	StableNuclide{name, half_life, activity}
+    }
+
     // fn new(name: String, half_life: f64, activity: f64) ->StableNuclide
     // {
     // 	StableNuclide{name, half_life, activity}
     // }
-    fn new(&self)
-    {
 
-    }
+    // fn new(&self)
+    // fn new(&self)->&dyn Nuclide
+    // fn new(&self, name: String, half_life: f64, activity: f64)->StableNuclide
+    // fn new(&self, name: String, half_life: f64, activity: f64)->dyn Nuclide
+    // // fn new(&self) //->StableNuclide
+    // {
+    // 	// StableNuclide {name, half_life, activity};
+    // 	Nuclide {name, half_life, activity};
+    // }
 
-    fn exp_law(&self, _time: f64)->f64
-    {
-	self.activity
-    }
+    // fn exp_law(&self, _time: f64)->f64
+    // {
+    // 	self.activity
+    // }
 
-    fn print_activity(&self, time: f64)
-    {
-	println!("Activity of {} is {}", self.name, self.exp_law(time));
-    }
+    // fn print_activity(&self, time: f64)
+    // {
+    // 	println!("Activity of {} is {}", self.name, self.exp_law(time));
+    // }
 
     // fn get_activity(nuclide: &dyn Nuclide, time: f64)
     // {
@@ -53,29 +69,29 @@ impl Nuclide for StableNuclide
     // }
 }
 
-impl Nuclide for RadioNuclide
-{
-    fn new(&self)
-    {
+// impl Nuclide for RadioNuclide
+// {
+//     fn new(&self)
+//     {
 
-    }
+//     }
 
-    fn exp_law(&self, time: f64)->f64
-    {
-	let a: f64=-f64::consts::LN_2*time/self.half_life;
-	self.activity*a.exp()
-    }
+//     fn exp_law(&self, time: f64)->f64
+//     {
+// 	let a: f64=-f64::consts::LN_2*time/self.half_life;
+// 	self.activity*a.exp()
+//     }
 
-    fn print_activity(&self, time: f64)
-    {
-	println!("Activity of {} is {}", self.name, self.exp_law(time));
-    }
-}
+//     fn print_activity(&self, time: f64)
+//     {
+// 	println!("Activity of {} is {}", self.name, self.exp_law(time));
+//     }
+// }
 
-    fn get_activity(nuclide: &dyn Nuclide, time: f64)
-    {
-	nuclide.print_activity(time);
-    }
+    // fn get_activity(nuclide: &dyn Nuclide, time: f64)
+    // {
+    // 	nuclide.print_activity(time);
+    // }
 
 
 // pub struct Particle
@@ -111,24 +127,22 @@ impl Nuclide for RadioNuclide
 fn main() {
     println!("Hello, world!");
 
-    let cs_137=new1(String::from("Cs-137"), 30.0, 50.0);
-    let ba_137m=RadioNuclide {name: String::from("Ba-137m"), half_life: 2.55, activity: 100.0,};
+    // let cs_137=new1(String::from("Cs-137"), 30.0, 50.0);
+    // let ba_137m=RadioNuclide {name: String::from("Ba-137m"), half_life: 2.55, activity: 100.0,};
     let ba_137=StableNuclide {name: String::from("Ba-137"), half_life: 0.0, activity: 100.0,};
 
-    // cs_137.print_activity(10.0);
-    // ba_137m.print_activity(1.0);
-    // ba_137.print_activity(10.0);
-    get_activity(&cs_137, 10.0);
-    get_activity(&ba_137m, 100.0);
-    get_activity(&ba_137, 10.0);
+
+    // get_activity(&cs_137, 10.0);
+    // get_activity(&ba_137m, 100.0);
+    // get_activity(&ba_137, 10.0);
 }
 
-fn new1(name: String, half_life: f64, activity: f64)->RadioNuclide
-{
-    RadioNuclide
-    {
-	name,
-	half_life,
-	activity,
-    }
-}
+// fn new1(name: String, half_life: f64, activity: f64)->RadioNuclide
+// {
+//     RadioNuclide
+//     {
+// 	name,
+// 	half_life,
+// 	activity,
+//     }
+// }
