64860cc902ef4297a1db533825f7a5eb4e22d0d6
pitkajuh <github_pj@protonmail.com>
2024-06-11 17:26:39 +0300

    Edit

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
+use std::vec::Vec;
 mod nuclide;
 mod decay;
-use std::vec::Vec;
-use crate::nuclide::StableNuclide;
-use crate::decay::DecayType;
-use crate::decay::DecayScheme;
-use crate::decay::BetaDecay;
-use crate::decay::InternalConversion;
 use crate::nuclide::NuclideType;
 use crate::nuclide::RadioNuclide;
+use crate::nuclide::StableNuclide;
+use crate::decay::DecayScheme;
+use crate::decay::DecayType;
+use crate::decay::BetaDecay;
+use crate::decay::InternalConversion;
 
 fn main() {
-    println!("Hello, world!");
     const HALF_LIFE: f32=30.2;
     let activity: f32=100.0;
+
     let cs_137=RadioNuclide{name: String::from("Cs-137"), half_life: HALF_LIFE};
     let ba_137m=RadioNuclide{name: String::from("Ba-137m"), half_life: 2.6};
     let ba_137=StableNuclide{name: String::from("Ba-137")};
@@ -29,6 +29,6 @@ fn main() {
     let cs_137_branch_2=DecayType::BetaDecayType(decay2);
     let cs_137_branch_3=DecayType::InternalConversionType(decay3);
 
-    let decays: Vec<DecayType>=vec![cs_137_branch_1, cs_137_branch_2, cs_137_branch_3];
+    let decays=vec![cs_137_branch_1, cs_137_branch_2, cs_137_branch_3];
     let cs_137_decay=DecayScheme{activity: activity, decays: decays};
 }
