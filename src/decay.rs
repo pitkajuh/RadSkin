0ea703bccb311febfa9670e8f35a867cfaf99f8f
pitkajuh <github_pj@protonmail.com>
2024-06-12 22:36:20 +0300

    Edit

--- src/decay.rs
+++ src/decay.rs
@@ -1,11 +1,29 @@
 use core::f32;
 use crate::nuclide::NuclideType;
+use crate::nuclide::RadioNuclide;
 
 pub enum DecayType<'a>
 {
     BetaDecayType(BetaDecay<'a>),
     InternalConversionType(InternalConversion<'a>),
 }
+
+// trait GetParent
+// {
+//     fn get_parent(&self)->RadioNuclide;
+//     // {
+
+//     // }
+// }
+
+
+// impl <'a>GetParent for DecayType<'a>
+// {
+//     fn get_parent(&self)->RadioNuclide
+//     {
+
+//     }
+// }
 
 fn exp_decay(activity: f32, half_life: f32, time: f32)->f32
 {
@@ -23,13 +41,20 @@ pub struct DecayScheme<'a>
 pub struct InternalConversion<'a>
 {
     pub probability: f32,
-    pub parent: &'a NuclideType,
-    pub daughter: &'a NuclideType,
+    pub parent: &'a RadioNuclide,
+    pub daughter: &'a NuclideType
 }
 
 pub struct BetaDecay<'a>
 {
     pub probability: f32,
-    pub parent: &'a NuclideType,
-    pub daughter: &'a NuclideType,
+    pub parent: &'a RadioNuclide,
+    pub daughter: &'a NuclideType
+}
+
+fn calc(decay: DecayType, activity: f32, time: f32)
+{
+    // let type_i=decay(_,parent);
+    // type_i.parent;
+    // let activity_t=exp_decay(activity, decay.)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::vec::Vec;
 mod nuclide;
 mod decay;
 use crate::nuclide::NuclideType;
@@ -13,22 +12,37 @@ fn main() {
     const HALF_LIFE: f32=30.2;
     let activity: f32=100.0;
 
-    let cs_137=RadioNuclide{name: String::from("Cs-137"), half_life: HALF_LIFE};
-    let ba_137m=RadioNuclide{name: String::from("Ba-137m"), half_life: 2.6};
-    let ba_137=StableNuclide{name: String::from("Ba-137")};
+    let cs_137=RadioNuclide{
+	name: String::from("Cs-137"),
+	half_life: HALF_LIFE};
+    let ba_137m=RadioNuclide{
+	name: String::from("Ba-137m"),
+	half_life: 2.6};
+    let ba_137=StableNuclide{
+	name: String::from("Ba-137")};
 
-    let parent1=NuclideType::RadioNuclideType(cs_137);
+    // let parent1=NuclideType::RadioNuclideType(cs_137);
     let daughter1=NuclideType::StableNuclideType(ba_137);
-    let parent2=NuclideType::RadioNuclideType(ba_137m);
+    // let parent2=NuclideType::RadioNuclideType(ba_137m);
 
-    let decay1=BetaDecay{probability: 0.054, parent: &parent1, daughter: &daughter1};
-    let decay2=BetaDecay{probability: 0.946, parent: &parent1, daughter: &daughter1};
-    let decay3=InternalConversion{probability: 1.0, parent: &parent2, daughter: &daughter1};
+    let decay1=BetaDecay{
+	probability: 0.054,
+	parent: &cs_137,
+	daughter: &daughter1};
+    // let decay2=BetaDecay{probability: 0.946, parent: &cs_137, daughter: &daughter1};
+    // let decay3=InternalConversion{probability: 1.0, parent: &ba_137m, daughter: &daughter1};
 
     let cs_137_branch_1=DecayType::BetaDecayType(decay1);
-    let cs_137_branch_2=DecayType::BetaDecayType(decay2);
-    let cs_137_branch_3=DecayType::InternalConversionType(decay3);
+    // let cs_137_branch_2=DecayType::BetaDecayType(decay2);
+    // let cs_137_branch_3=DecayType::InternalConversionType(decay3);
 
-    let decays=vec![cs_137_branch_1, cs_137_branch_2, cs_137_branch_3];
-    let cs_137_decay=DecayScheme{activity: activity, decays: decays};
+    // let decays=vec![cs_137_branch_1, cs_137_branch_2, cs_137_branch_3];
+    // let cs_137_decay=DecayScheme{activity: activity, decays: decays};
+
+    // for i in &decays
+    // {
+
+
+    // }
+
 }
