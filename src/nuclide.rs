3d3a233abf81f9876313ad2039db7ffb58ab6a23
pitkajuh <github_pj@protonmail.com>
2024-06-16 22:14:48 +0300

    Edit

--- src/nuclide.rs
+++ src/nuclide.rs
@@ -1,26 +1,5 @@
 use core::f32;
 
-// enum NuclideType2{
-//     StableNuclide,
-//     RadioNuclide
-// }
-
-// struct Nuclide{
-//     name: String,
-//     nuclide_type: NuclideType2,
-//     half_life: f32
-// }
-
-// pub enum NuclideType1{
-//     RadioNuclideType1(String, f32),
-//     StableNuclideType1(String),
-// }
-
-// pub struct RadioNuclideType2{
-//     pub name: String,
-//     pub half_life: f32,
-// }
-
 pub enum NuclideType {
     RadioNuclideType(RadioNuclide),
     StableNuclideType(StableNuclide),
